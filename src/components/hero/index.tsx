e35eac4276b72c2fc954b7033136d40954b62aff
AdamDemian <adamdemian1@gmail.com>
2023-03-07 23:01:10 +0100

    header & footer done

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,20 +1,26 @@
+import Image from "next/image";
 import Link from "next/link";
 
 const Hero = () => {
   return (
     <div className="bg-black text-white w-full">
       <section
-        className="container grid justify-center gap-6 py-8 md:py-12 lg:py-36 felx-wrap flex-col items-center mt-24 px-4 fadeInUp mx-auto w-full text-center"
-        data-wow-delay=".2s"
+        className="h-[80vh] container grid justify-center items-center gap-6 py-8 md:py-12 lg:py-36 felx-wrap flex-col mt-24 fadeInUp mx-auto w-full text-center"
+        data-wow-delay=".2s relative"
       >
-        <h1 className="mb-5 text-3xl text-red font-satoshiB font-extrabold leading-tight dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-          Liuco
-        </h1>
-        <h1 className="mb-5 text-3xl font-satoshiM leading-tight text-white dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-          Create Ecommerce The Better Way
-        </h1>
+        <div className="lg:flex">
+          <div className="lg:w-1/2">
+            <h1 className="mb-5 text-4xl text-left font-satoshiM leading-tight text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+              Create <span className="underline text-red">Ecommerce</span><br />The Better Way
+            </h1>
+          </div>
+          <div className="z-0 lg:w-1/2 opacity-50 absolute left-[2%] overflow-hidden top-[25%]">
+            <div className="lg:relative w-[23rem] h-[25rem] md:h-[33rem] md:w-[35rem] ">
+              <Image src={'/images/hero-image.svg'} alt='hero image' fill className="object-fill" />
+            </div>
+          </div>
+        </div>
       </section>
-      <hr className="border-slate-200" />
     </div>
   );
 };
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,12 +1,39 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="text-center h-16 sm:h-20 w-3/4 sm:pt-2 pt-4 border-t-2 border-gray-400 dark:border-stone-700 mt-5 flex sm:flex-row flex-col justify-between items-center px-16 space-y-3 sm:mb-0 mb-10">
-      <div className="text-black dark:text-white">
-        <p className="text-sm sm:text-base">
-          © {new Date().getFullYear()} Liuco. All rights reserved.
-        </p>
+    <footer className="w-full h-[87vh] flex bg-black text-white overflow-hidden">
+      <div className="w-full h-full m-0 p-0 flex flex-col md:flex-row">
+        <div className="grid w-full md:text-lg grid-rows-1 gap-28 grid-cols-2 md:grid-cols-1 mt-28 mb-16 mx-auto items-center ">
+          <div className="md:justify-self-center justify-self-center md:w-28">
+            <Link href={''} className='hover:text-coolGray duration-150 mb-2'>Sign In</Link>
+            <p>info@liuco.com</p>
+          </div>
+          <div className="flex flex-col justify-self-center text-left md:w-28">
+            <Link href={''} className="hover:text-coolGray duration-150 mb-2">Twitter</Link>
+            <Link href={''} className="hover:text-coolGray duration-150 mb-2">Instagram</Link>
+            <Link href={''} className="hover:text-coolGray duration-150 mb-2">Linkedln</Link>
+          </div>
+          <div className="justify-self-center text-left md:justify-self-center md:w-28 ">
+            <p>© {new Date().getFullYear()} Liuco</p>
+          </div>
+        </div>
+        <div className="relative flex flex-col w-full h-full overflow-hidden order-last md:order-first">
+          <Link href={'/signin'} className={'w-[18.5rem] h-[15rem] bottom-1 absolute'}>
+            <div className="absolute h-full w-[20%] top z-20">
+              <div className="relative h-12 md:h-20 lg:w-20 inset-y-[2rem] inset-x-[8rem] lg:inset-y-[3rem] lg:inset-x-[12rem]">
+                <Image src={'/icons/arrow-up-right.svg'} alt='arrow' fill className="object-fill" />
+              </div>
+              <h1 className="text-5xl lg:text-7xl absolute bottom-16 md:bottom-10 ml-5 md:ml-10">Get <br /> Started</h1>
+            </div>
+            <div className="relative inset-y-[-2rem] inset-x-[-3.5rem] md:inset-y-[-5rem] md:inset-x-[-5rem] opacity-50 hover:opacity-100 duration-200 z-[100]">
+              <div className="relative h-[22rem] w-[22rem] md:w-[33rem] md:h-[33rem]">
+                <Image src={'/images/elipse.svg'} alt='elipse' fill className="object-fill" />
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
     </footer>
   );
